23327438cabfdc7cd4c48d974ca242915a789781
PrinceDevHub <rayprince2004@gmail.com>
2025-11-30 00:55:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
+const goalText = document.getElementById('goal-text');
+
+updateBigCup(); 
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener('click', () => {
+    highlightCups(idx);
+  });
+});
+
+function highlightCups(idx) {
+
+  if (idx === smallCups.length - 1 && smallCups[idx].classList.contains('full')) {
+    return;
+  }
+
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add('full');
+    } else {
+      cup.classList.remove('full');
+    }
+  });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = 'hidden';
+    remained.style.height = 0;
+    goalText.innerText = 'Goal: Completed';
+  } else {
+    remained.style.visibility = 'visible';
+    remained.style.height = `${2 - (0.0025 * fullCups)}L`; // Adjusted for a better visual representation
+    goalText.innerText = 'Goal: 2 liters';
+  }
+}
